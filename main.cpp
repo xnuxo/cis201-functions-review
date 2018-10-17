11c3c5741f97bbd5085af27d4f410d03834f1243
Jude McParland <judemcparland@cis.vvc.edu>
2018-10-16 20:11:11 -0700

    update

--- main.cpp
+++ main.cpp
@@ -16,33 +16,26 @@ bool is_a(char a);
 
 int main()
 {
-
-	string in;
+	string in; // Delcare input string var.
 	
-
 	cout << "Please enter a string: ";
 	getline(cin, in);
-	int out = detect_as(in);
-	cout << "The number of A's in the string is: " << out << endl;
+	cout << "The number of A's in the string is: " << detect_as(in) << endl;
 	return 0;
 }
 
 bool is_a(char a)
 {
-	return a == 'a' || a == 'A';
+	return a == 'a' || a == 'A'; // Detect if passed char is an upper or lower case a.
 }
 
 int detect_as(string s)
 {
 	int count = 0;
-	for(int x = 0; x < s.length(); x++)
+	for(int x = 0; x < s.length(); x++) // Run the loop for as long as the string is.
 	{
-	
-		if(is_a(s.at(x)))
-		{
+		if(is_a(s.at(x))) // Passes the current charater of the string to detect if is a or not. If it is, update the count by 1.
 			count++;
-		}
-	
 	}
 	return count;
 }
